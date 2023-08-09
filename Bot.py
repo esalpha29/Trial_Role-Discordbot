20c52b12a60889cae765068dd548437b62804198
ES ALPHA <112908559+esalpha29@users.noreply.github.com>
2023-08-09 23:20:19 +0500

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,23 @@
+# Free Trial Role Custom Discord Bot
+
+---
+
+##  👾 Features:
+
+- ✓ Set customizable trial durations
+- ✓ Automatic role assignment & removal
+- ✓ User-friendly commands and interface
+- ✓ Custom Bot Status
+
+---
+
+
+⭐ If you want to get this working Bot or just the bot code then join my Discord Server and open a ticket. 
+
+**📌 Discord Server:**  https://discord.gg/HWKsXcMazv
+
+**🌐 My Site:**  https://alphadiscord.com/
+
+<br> </br>
+
+```Copyright (c) 2023 esalpha. All rights reserved.```
